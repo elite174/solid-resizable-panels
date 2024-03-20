738a2a3d91b408b6b4860bb2e9483e17c40b2575
Vladislav Lipatov <lipatov.xex@gmail.com>
2024-03-20 20:46:43 +0700

    Export extra types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
-export { PanelGroup } from './PanelGroup';
-export { Panel } from './Panel';
-export { ResizeHandle } from './ResizeHandle';
+export { PanelGroup } from "./PanelGroup";
+export { Panel } from "./Panel";
+export { ResizeHandle } from "./ResizeHandle";
 
-export type { PanelProps } from './Panel';
-export type { PanelGroupProps } from './PanelGroup';
-export type { ResizeHandleProps } from './ResizeHandle';
+export type { PanelProps } from "./Panel";
+export type { PanelGroupProps, Logger, PanelGroupAPI } from "./PanelGroup";
+export type { ResizeHandleProps } from "./ResizeHandle";
+export type { Direction, LayoutItem, ResolvedLayoutItem } from "./types";
